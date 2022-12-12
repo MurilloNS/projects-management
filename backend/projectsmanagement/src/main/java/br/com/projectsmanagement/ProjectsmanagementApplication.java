0b58885e192a3a0e9bb1fa24eff53e0ff62bda15
MurilloNS <murillo.murillo2001@gmail.com>
2022-12-12 09:40:48 -0300

    first commit

---
+++ backend/projectsmanagement/src/main/java/br/com/projectsmanagement/ProjectsmanagementApplication.java
@@ -0,0 +1,13 @@
+package br.com.projectsmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectsmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectsmanagementApplication.class, args);
+	}
+
+}
---
+++ backend/projectsmanagement/src/test/java/br/com/projectsmanagement/ProjectsmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projectsmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectsmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
