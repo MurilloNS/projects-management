720f04c3f50adde43b82a13585255402e09dfeba
MurilloNS <murillo.murillo2001@gmail.com>
2022-12-20 11:15:49 -0300

    FindById Test Implemented

--- backend/projectsmanagement/src/test/java/br/com/projectsmanagement/controllers/UserControllerTest.java
+++ backend/projectsmanagement/src/test/java/br/com/projectsmanagement/controllers/UserControllerTest.java
@@ -1,14 +1,40 @@
 package br.com.projectsmanagement.controllers;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.when;
+
+import java.util.Date;
+import java.util.Optional;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 
+import br.com.projectsmanagement.entities.User;
+import br.com.projectsmanagement.services.impl.UserServiceImpl;
+
+@SpringBootTest
 class UserControllerTest {
+	private User user;
+	private Optional<User> optionalUser;
+
+	@InjectMocks
+	private UserController userController;
+
+	@Mock
+	private UserServiceImpl userServiceImpl;
 
 	@BeforeEach
-	static void setUp() {
+	void setUp() {
+		MockitoAnnotations.openMocks(this);
+		startUser();
 	}
 
 	@Test
@@ -17,8 +43,16 @@ class UserControllerTest {
 	}
 
 	@Test
-	void testGetUserById() {
-		fail("Not yet implemented");
+	void whenFindByIdThenReturnSuccess() {
+		when(userServiceImpl.getUserById(anyLong())).thenReturn(optionalUser);
+
+		ResponseEntity<Optional<User>> response = userController.getUserById(1L);
+
+		assertNotNull(response);
+		assertNotNull(response.getBody());
+		assertEquals(ResponseEntity.class, response.getClass());
+		assertEquals(optionalUser.getClass(), response.getBody().getClass());
+		assertEquals(optionalUser.get().getId(), response.getBody().get().getId());
 	}
 
 	@Test
@@ -36,4 +70,8 @@ class UserControllerTest {
 		fail("Not yet implemented");
 	}
 
+	private void startUser() {
+		user = new User("Valdir", "valdir@gmail.com", "123456", new Date());
+		optionalUser = Optional.of(new User("Valdir", "valdir@gmail.com", "123456", new Date()));
+	}
 }
