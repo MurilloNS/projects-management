a37dec13c7c2abf26b89b348607828f496a79115
MurilloNS <murillo.murillo2001@gmail.com>
2022-12-28 11:24:06 -0300

    CRUD Project Created

--- backend/projectsmanagement/src/main/java/br/com/projectsmanagement/controllers/ProjectController.java
+++ backend/projectsmanagement/src/main/java/br/com/projectsmanagement/controllers/ProjectController.java
@@ -1,9 +1,52 @@
 package br.com.projectsmanagement.controllers;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.projectsmanagement.entities.Project;
+import br.com.projectsmanagement.services.ProjectService;
+
 @RestController
-@RequestMapping("/project")
+@RequestMapping("/projeto")
 public class ProjectController {
+	@Autowired
+	private ProjectService projectService;
+
+	@GetMapping("/listar")
+	public ResponseEntity<List<Project>> listAll() {
+		return ResponseEntity.ok(projectService.listAll());
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Optional<Project>> getProjectById(@PathVariable Long id) {
+		return ResponseEntity.ok(projectService.getProjectById(id));
+	}
+
+	@PostMapping("/cadastrar")
+	public ResponseEntity<Project> registerProject(@RequestBody Project project) {
+		return new ResponseEntity<>(projectService.registerProject(project), HttpStatus.CREATED);
+	}
+
+	@PutMapping("/atualizar/{id}")
+	public ResponseEntity<Project> updateProject(@PathVariable Long id, @RequestBody Project project) {
+		return ResponseEntity.ok(projectService.updateProject(id, project));
+	}
+
+	@DeleteMapping("/excluir/{id}")
+	public ResponseEntity<Void> deleteProject(@PathVariable Long id) {
+		projectService.deleteProject(id);
+		return ResponseEntity.noContent().build();
+	}
 }
--- backend/projectsmanagement/src/main/java/br/com/projectsmanagement/services/impl/ProjectServiceImpl.java
+++ backend/projectsmanagement/src/main/java/br/com/projectsmanagement/services/impl/ProjectServiceImpl.java
@@ -1,8 +1,15 @@
 package br.com.projectsmanagement.services.impl;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import br.com.projectsmanagement.entities.Project;
+import br.com.projectsmanagement.exception.InvalidIdException;
 import br.com.projectsmanagement.repositories.ProjectRepository;
 import br.com.projectsmanagement.services.ProjectService;
 
@@ -10,4 +17,40 @@ import br.com.projectsmanagement.services.ProjectService;
 public class ProjectServiceImpl implements ProjectService {
 	@Autowired
 	private ProjectRepository projectRepository;
+
+	@Override
+	public List<Project> listAll() {
+		return projectRepository.findAll();
+	}
+
+	@Override
+	public Optional<Project> getProjectById(Long id) {
+		return projectRepository.findById(id);
+	}
+
+	@Override
+	public Project registerProject(Project project) {
+		return projectRepository.saveAndFlush(project);
+	}
+
+	@Override
+	public Project updateProject(Long id, Project project) {
+		try {
+			Project projectUpdated = projectRepository.findById(id).get();
+			projectUpdated.setName(project.getName());
+			projectUpdated.setDescription(project.getDescription());
+			return projectRepository.saveAndFlush(projectUpdated);
+		} catch (NoSuchElementException e) {
+			throw new InvalidIdException("Projeto não encontrado!");
+		}
+	}
+
+	@Override
+	public void deleteProject(Long id) {
+		try {
+			projectRepository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new InvalidIdException("Esse projeto já não existe!");
+		}
+	}
 }
