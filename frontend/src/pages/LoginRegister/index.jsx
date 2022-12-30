dc87ba43460c4c87761e1319fc8f0b8290341bbf
MurilloNS <murillo.murillo2001@gmail.com>
2022-12-30 13:28:41 -0300

    file

--- frontend/src/pages/LoginRegister/index.jsx
+++ frontend/src/pages/LoginRegister/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   Container,
   Box,
@@ -7,10 +8,36 @@ import {
   Forget,
   Button,
   Divisor,
-  DivisorRegister
+  DivisorRegister,
 } from "./styles";
+import http from "../../apis/http";
+import Swal from "sweetalert2";
 
 export default function LoginRegister() {
+  const [user, setUser] = useState({ name: "", email: "", password: "" });
+
+  const handleChange = (e) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    http.post("/usuario/cadastrar", user).then((result) => {
+      Swal.fire({
+        position: "center",
+        icon: "success",
+        title: "Usuário cadastrado",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+    }).catch((e) => {
+      Swal.fire({
+        icon: 'error',
+        text: e.response.data.message,
+      })
+    });
+  };
+
   return (
     <Container>
       <Box>
@@ -22,11 +49,29 @@ export default function LoginRegister() {
           <Button type="submit">Logar</Button>
         </BoxLogin>
         <Divisor />
-        <BoxLogin>
+        <BoxLogin onSubmit={handleSubmit}>
           <Title>Criar conta</Title>
-          <Input type="text" placeholder="Insira seu nome" />
-          <Input type="email" placeholder="Insira seu e-mail" />
-          <Input type="password" placeholder="Insira sua senha" />
+          <Input
+            onChange={handleChange}
+            value={user.name}
+            name="name"
+            type="text"
+            placeholder="Insira seu nome"
+          />
+          <Input
+            onChange={handleChange}
+            value={user.email}
+            name="email"
+            type="email"
+            placeholder="Insira seu e-mail"
+          />
+          <Input
+            onChange={handleChange}
+            value={user.password}
+            name="password"
+            type="password"
+            placeholder="Insira sua senha"
+          />
           <DivisorRegister />
           <Button type="submit">Enviar</Button>
         </BoxLogin>
